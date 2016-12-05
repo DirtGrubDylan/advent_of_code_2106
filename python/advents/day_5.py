5e071458792ba55b3165dc9d02f33c3bc4ed1fcf
Dylan Hicks <dman1366@gmail.com>
2016-12-05 15:10:33 -0700

    Finished day 5.

--- python/advents/day_5.py
+++ python/advents/day_5.py
@@ -14,7 +14,7 @@ class Door(object):
         self.door_id = door_id
         self.code_index = 0
 
-    def next_id_hash_character(self):
+    def next_id_hash(self):
         """Returns the sixth character from the next md5 hashed binary of the
            door_id combined with the code_index, if the hashed binary starts
            with five leading zeroes.
@@ -26,21 +26,23 @@ class Door(object):
             (str): The sixth character of the next md5 hash value of the door_id
                    combined with the code_index.
         """
-        first_six_hex_hash_characters = ''
+        hex_hash = ''
 
-        while first_six_hex_hash_characters[:5] != '00000':
-            combined_hash_input = (self.door_id + str(self.code_index)).encode()
+        while hex_hash[:5] != '00000':
+            combined_hash_input = (
+                self.door_id + str(self.code_index)).encode(encoding='utf_8')
 
-            md5_hasher = hashlib.md5()
-            md5_hasher.update(combined_hash_input)
-            first_six_hex_hash_characters = md5_hasher.hexdigest()[:6]
+            hex_hash = hashlib.md5(combined_hash_input).hexdigest()
 
             self.code_index += 1
 
-        return first_six_hex_hash_characters[-1]
+        return hex_hash
 
     def code_from_id(self):
         """Returns the door code from the door_id.
+
+        Note:
+            Solved left to right.
 
         Args:
             N/A
@@ -51,9 +53,41 @@ class Door(object):
         code = ''
 
         for _ in range(8):
-            code += self.next_id_hash_character()
+            code += self.next_id_hash()[5]
 
         return code
+
+    def code_from_id_new(self, print_iterations=False):
+        """Returns the door code from the door_id.
+
+        Note:
+            Solved in place.
+
+        Args:
+            print_iterations (bool): Indicates whether you want cinema printing.
+
+        Returns:
+            (str): The door code.
+        """
+        code = ['_' for _ in range(8)]
+        num_of_symbols = 0
+        possible_indicies = set([str(num) for num in range(8)])
+
+        while num_of_symbols < 8:
+            index, code_symbol = self.next_id_hash()[5:7]
+
+            if index in possible_indicies:
+                index = int(index)
+
+                if code[index] == '_':
+                    num_of_symbols += 1
+                    code[int(index)] = code_symbol
+
+            if print_iterations:
+                print(
+                    'Finding new door code: {}'.format(''.join(code)), end='\r')
+
+        return ''.join(code)
 
 
 def main():
@@ -65,6 +99,10 @@ def main():
     start_time = datetime.now()
 
     print('The door code is: {}'.format(door.code_from_id()))
+
+    door.code_index = 0
+
+    print('\nThe new door code is: {}'.format(door.code_from_id_new(True)))
 
     print(
         'Time to decode door_id [{}]: {}s'.format(
--- python/tests/test_day5.py
+++ python/tests/test_day5.py
@@ -17,8 +17,8 @@ class TestDay5(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_next_id_hash_character(self):
-        """Unit test the next_id_hash_character function of the Door class.
+    def test_next_id_hash(self):
+        """Unit test the next_id_hash function of the Door class.
 
         Args:
             N/A
@@ -26,9 +26,9 @@ class TestDay5(unittest.TestCase):
         Returns:
             N/A
         """
-        self.assertEqual(self.test_door.next_id_hash_character(), '1')
-        self.assertEqual(self.test_door.next_id_hash_character(), '8')
-        self.assertEqual(self.test_door.next_id_hash_character(), 'f')
+        self.assertEqual(self.test_door.next_id_hash()[5], '1')
+        self.assertEqual(self.test_door.next_id_hash()[5], '8')
+        self.assertEqual(self.test_door.next_id_hash()[5], 'f')
 
     def test_code_from_id(self):
         """Unit test the code_from_id function of the Door class.
@@ -40,6 +40,17 @@ class TestDay5(unittest.TestCase):
             N/A
         """
         self.assertEqual(self.test_door.code_from_id(), '18f47a30')
+
+    def test_code_from_id_new(self):
+        """Unit test the code_from_id_new function of the Door class.
+
+        Args:
+            N/A
+
+        Returns:
+            N/A
+        """
+        self.assertEqual(self.test_door.code_from_id_new(), '05ace8e3')
 
 
 if __name__ == "__main__":
