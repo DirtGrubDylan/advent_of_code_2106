7de019867195051b4d31199059560c885e7a0e5f
Dylan Hicks <dman1366@gmail.com>
2016-12-05 23:10:29 -0700

    More rust.

--- rust/day_1/src/main.rs
+++ rust/day_1/src/main.rs
@@ -1,23 +1,29 @@
 use std::env;
-use std::io::Read;
 use std::fs::File;
+use std::path::Path;
+use std::io::BufRead;
 
 fn main() {
+    let mut temp: Vec<String> = Vec::new();
     let mut data_path: std::path::PathBuf = env::current_dir().unwrap();
     data_path.push("data");
     data_path.push("input.txt");
 
-    data_from_file(data_path);
+    println!("The path is: {}", data_path.display());
+
+    let data: std::io::Lines<std::io::BufReader<File>> =
+        data_from_file(&data_path).unwrap();
+
+    for data_line in data {
+        temp = data_line.unwrap().split(',').map(|s| s.to_owned()).collect();
+    }
+
+    println!("{:?}", temp);
 }
 
-fn data_from_file(file_name: std::path::PathBuf) {
-    println!("The path is: {}", file_name.display());
+fn data_from_file<P: AsRef<Path>>(file_name: &P) ->
+        Result<std::io::Lines<std::io::BufReader<File>>, std::io::Error> {
+    let data_file: File = File::open(file_name)?;
 
-    let mut data_file = File::open(file_name).unwrap();
-
-    let mut line = String::new();
-
-    data_file.read_to_string(&mut line).unwrap();
-
-    println!("The data is: {}", line);
+    Ok(std::io::BufReader::new(data_file).lines())
 }
