683a96aae1e081f2a64004c9bace440733dec9e2
Dylan Hicks <dman1366@gmail.com>
2016-12-09 10:26:11 -0700

    Day 3 part 1 in Rust finished.

--- rust/day_3/src/bin/bin.rs
+++ rust/day_3/src/bin/bin.rs
@@ -1,5 +1,15 @@
 extern crate day_3;
 
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let mut data_path: PathBuf = env::current_dir().unwrap();
+    data_path.push("data");
+    data_path.push("input.txt");
+
+    let number_of_valid_row_major_triangles_in_file: u32 =
+        day_3::number_of_valid_row_major_triangles_in_file(&data_path).unwrap();
+
+    println!("Answer 1: {}", number_of_valid_row_major_triangles_in_file);
 }
--- rust/day_3/src/lib.rs
+++ rust/day_3/src/lib.rs
@@ -5,24 +5,19 @@ use std::io::BufRead;
 use triangle::Triangle;
 
 pub mod triangle {
-    #[derive(Debug)]
-    pub struct Triangle {
-        pub side_a: i32,
-        pub side_b: i32,
-        pub side_c: i32
-    }
+    #[derive(Debug, PartialEq)]
+    pub struct Triangle(pub i32, pub i32, pub i32);
 
     impl Triangle {
         pub fn is_valid(&self) -> bool {
-            (self.side_a + self.side_b > self.side_c) &&
-            (self.side_a + self.side_c > self.side_b) &&
-            (self.side_b + self.side_c > self.side_a)
+            (self.0 + self.1 > self.2) && (self.0 + self.2 > self.1) &&
+            (self.1 + self.2 > self.0)
         }
     }
 }
 
-pub fn read_triangles_from_file<P>(file_name: &P)
-        -> Result<Vec<Vec<Triangle>>, String> where P: AsRef<Path> {
+pub fn read_row_major_triangles_from_file<P>(file_name: &P)
+        -> Result<Vec<Triangle>, String> where P: AsRef<Path> {
     let data_file: File = File::open(file_name).map_err(|e| e.to_string())?;
     let mut data: Vec<Triangle> = Vec::new();
 
@@ -31,16 +26,29 @@ pub fn read_triangles_from_file<P>(file_name: &P)
         let triangle_sides: Vec<i32> =
             triangle_sides.split_whitespace().collect::<Vec<&str>>().iter()
                           .map(|&s| s.parse::<i32>().unwrap()).collect();
-        let triangle: Triangle = Triangle {
-            side_a: triangle_sides[0],
-            side_b: triangle_sides[1],
-            side_c: triangle_sides[2]
-        };
+
+        let triangle: Triangle = Triangle(
+            triangle_sides[0], triangle_sides[1], triangle_sides[2]);
 
         data.push(triangle);
     }
 
     Ok(data)
+}
+
+pub fn number_of_valid_row_major_triangles_in_file<P>(file_name: &P)
+        -> Result<u32, String> where P: AsRef<Path> {
+    let triangles: Vec<Triangle> =
+        read_row_major_triangles_from_file(file_name)?;
+    let mut number_valid: u32 = 0;
+
+    for triangle in triangles {
+        if triangle.is_valid() {
+            number_valid += 1;
+        }
+    }
+
+    Ok(number_valid)
 }
 
 
@@ -57,14 +65,26 @@ mod tests {
         data_path.push("data");
         data_path.push("input.txt");
 
-        let data: Vec<Vec<i32>> = read_triangles_from_file(&data_path).unwrap();
+        let data: Vec<Triangle> =
+            read_row_major_triangles_from_file(&data_path).unwrap();
 
-        let answer: Triangle = Triangle {
-            side_a: data[0][0],
-            side_b: data[0][1],
-            side_c: data[0][2]
-        };
+        let answer:Triangle = Triangle(775, 785, 361);
 
         assert_eq!(data[0], answer);
+    }
+
+    #[test]
+    fn test_triangle_is_valid() {
+        let data:Triangle = Triangle(775, 785, 361);
+
+        assert!(data.is_valid());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_triangle_is_not_valid() {
+        let data: Triangle = Triangle(622, 375, 125);
+
+        assert!(data.is_valid());
     }
 }
