a48a4f8036fd71c9644109b7e594b4dba4de4c81
Dylan Hicks <dman1366@gmail.com>
2017-01-14 12:52:52 -0700

    Finished Day 5!

--- rust/day_5/src/lib.rs
+++ rust/day_5/src/lib.rs
@@ -1,5 +1,7 @@
 extern crate crypto;
 
+use std::str::Chars;
+
 use crypto::md5;
 use crypto::digest::Digest;
 
@@ -34,6 +36,43 @@ impl Door {
 
         password
     }
+
+
+    pub fn in_place_password(&self, printing: bool) -> String {
+        let mut iteration: u32 = 0;
+        let mut number_of_assigned_characters: u32 = 0;
+        let mut password: Vec<char> = vec!['_', '_', '_', '_', '_', '_', '_', '_'];
+
+        while number_of_assigned_characters < 8 {
+            let hashed_id: String = self.hash_id_with_iteration(iteration);
+
+
+            if hashed_id.starts_with("00000") {
+                let mut hashed_id: Chars = hashed_id.chars();
+
+                let index: u32 = hashed_id.nth(5).unwrap_or('_').to_digit(10).unwrap_or(8);
+
+                if let Some(element) = password.get_mut(index as usize) {
+                    if *element == '_' {
+                        *element = hashed_id.nth(0).unwrap_or('_');
+                        number_of_assigned_characters += 1;
+                    }
+                }
+            }
+
+            if printing {
+                print!("Password: {:?}\r", password);
+            }
+
+            iteration += 1;
+        }
+
+        if printing {
+            println!("");
+        }
+
+        password.into_iter().collect::<String>()
+    }
 }
 
 
@@ -46,8 +85,9 @@ mod test {
         let test_door: Door = Door("abc".to_string());
 
         assert!(test_door.hash_id_with_iteration(5017308).starts_with("000008f82"));
-        assert!(test_door.hash_id_with_iteration(3231929).starts_with("000001"));
+        assert!(test_door.hash_id_with_iteration(3231929).starts_with("0000015"));
         assert!(test_door.hash_id_with_iteration(5278568).starts_with("00000f"));
+        assert!(test_door.hash_id_with_iteration(5357525).starts_with("000004e"));
     }
 
 
@@ -56,5 +96,13 @@ mod test {
         let test_door: Door = Door("abc".to_string());
 
         assert_eq!(test_door.l_to_r_password(), "18f47a30");
+    }
+
+
+    #[test]
+    fn test_in_place_password() {
+        let test_door: Door = Door("abc".to_string());
+
+        assert_eq!(test_door.in_place_password(false), "05ace8e3");
     }
 }
--- rust/day_5/src/main.rs
+++ rust/day_5/src/main.rs
@@ -2,8 +2,19 @@ extern crate day_5;
 
 use day_5::Door;
 
+use std::env;
+use std::time::Instant;
+
 fn main() {
-    let locked_door: Door = Door("wtnhxymk".to_string());
+    let arguments: Vec<String> = env::args().collect();
+    let time_now: Instant = Instant::now();
+    let locked_door: Door = Door(arguments[1].clone());
 
     println!("The First Password is: {}", locked_door.l_to_r_password());
+
+    let new_password: String = locked_door.in_place_password(false);
+
+    println!("The Second Password is: {}", new_password);
+
+    println!("Time to decode passwords: {}", time_now.elapsed().as_secs());
 }
