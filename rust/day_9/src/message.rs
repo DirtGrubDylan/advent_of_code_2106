49dcc324907f3e675c47a1e4dfc8acd30c99897f
Dylan Hicks <dman1366@gmail.com>
2017-02-06 11:00:55 -0700

    Day 9, part 1 finished.

--- rust/day_9/src/message.rs
+++ rust/day_9/src/message.rs
@@ -23,5 +23,27 @@ impl Message {
 
 
 fn decompress_string_v1(message: &str, marker_regex: Regex) -> String {
-    message.to_string()
+    let mut decompressed_message = String::new();
+    let mut str_index: usize = 0;
+
+    while let Some(matched) = marker_regex.find(&message[str_index..]) {
+        let captured = marker_regex.captures(&message[str_index..]).unwrap();
+
+        decompressed_message += &message[str_index..(str_index + matched.start())];
+
+        let number_of_chars: usize = (&captured[1]).parse().unwrap();
+        let number_of_repeats: usize = (&captured[2]).parse().unwrap();
+
+        str_index += matched.end() + number_of_chars;
+
+        for _ in 0..number_of_repeats {
+            decompressed_message += &message[(str_index - number_of_chars)..str_index];
+        }
+    }
+
+    if str_index < message.len() {
+        decompressed_message += &message[str_index.. message.len()];
+    }
+
+    decompressed_message
 }
